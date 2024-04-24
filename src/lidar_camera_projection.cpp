cf4874c0a339c4adc0ad9f92b8a22dce00ecbd28
sweunwave <sweunwave@gmail.com>
2024-04-24 21:34:14 +0900

    add passThrough filter on cpp

--- src/lidar_camera_projection.cpp
+++ src/lidar_camera_projection.cpp
@@ -2,6 +2,7 @@
 #include <pcl/point_types.h>
 #include <pcl/PCLPointCloud2.h>
 #include <pcl/conversions.h>
+#include <pcl/filters/passthrough.h>
 
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/image.hpp"
@@ -38,6 +39,11 @@ public:
             "/ouster/points",
             10,
             std::bind(&MinimalImagePublisher::lidar_callback, this, std::placeholders::_1));
+
+        filtered_lidar_pub_ = this->create_publisher<sensor_msgs::msg::PointCloud2>(
+            "/calibration/filtered_points",
+            10
+            );
 
         std::string calibration_params_path;
         this->declare_parameter<std::string>("calibration_params_path");
@@ -81,7 +87,7 @@ private:
 
         if (is_lidar) {
             for (const auto &point : projection_points){
-                cv::circle(cv_ptr->image, point.inRange_points, 1, point.color, 1);
+                cv::circle(cv_ptr->image, point.inRange_points, 2, point.color, -1);
             }
         }
         cv::imshow("projection_image", cv_ptr->image);
@@ -95,23 +101,49 @@ private:
         *ptr_cloud = cloud_dst;
 
         // std::chrono::system_clock::time_point start = std::chrono::system_clock::now();
+        
+        pcl::PointCloud<pcl::PointXYZI>::Ptr output(new pcl::PointCloud<pcl::PointXYZI>);
+        pcl::PointCloud<pcl::PointXYZI> dst;
+        
+        // @todo : 
+        pcl::PassThrough<pcl::PointXYZI> filter;
+        filter.setInputCloud(ptr_cloud);
+        filter.setFilterFieldName("y");
+        filter.setFilterLimits(-2, 2);
+        filter.filter(*output);
+
+        filter.setInputCloud(output);
+        filter.setFilterFieldName("x");
+        filter.setFilterLimits(-7, 0);
+        filter.filter(*output);
+
+        filter.setInputCloud(output);
+        filter.setFilterFieldName("z");
+        filter.setFilterLimits(-2, 1);
+        filter.filter(*output);
+
+        dst = *output;
+
+        auto pcd2_msg = cloud2cloudmsg(dst);
+        filtered_lidar_pub_->publish(pcd2_msg);
 
         std::vector<cv::Point3f> points_3d;
         std::vector<float> intensities;
-        points_3d.reserve(cloud_dst.size());
-        intensities.reserve(cloud_dst.size());
+        points_3d.reserve(dst.size());
+        intensities.reserve(dst.size());
 
         // convert pcl to cv vertor
-        std::transform(cloud_dst.begin(), cloud_dst.end(), std::back_inserter(points_3d),
+        std::transform(dst.begin(), dst.end(), std::back_inserter(points_3d),
                        [](const pcl::PointXYZI& point) {
                            return cv::Point3f(point.x, point.y, point.z);
                        });
 
         // get intensity vector
-        std::transform(cloud_dst.begin(), cloud_dst.end(), std::back_inserter(intensities),
+        std::transform(dst.begin(), dst.end(), std::back_inserter(intensities),
                        [](const pcl::PointXYZI& point) {
                            return point.intensity;
                        });
+        auto minmax = std::minmax_element(intensities.begin(), intensities.end());
 
         // std::chrono::duration<double>sec = std::chrono::system_clock::now() - start;
         // std::cout << "실행 시간(초) : " << sec.count() <<" seconds"<< std::endl;        
@@ -121,26 +153,30 @@ private:
 
         if (is_img){
             projection_points.clear();
-            auto minmax = std::minmax_element(intensities.begin(), intensities.end());
-            std::cout << "min: " << *(minmax.first) << std::endl;
-            std::cout << "max: " << *(minmax.second) << std::endl;
-            // std::cout << "rows : " << img_points.rows << " cols : " << img_points.cols << std::endl;
+            std::chrono::system_clock::time_point start = std::chrono::system_clock::now();
             for (int i = 0; i < img_points.rows; ++i) {
                 for (int j = 0; j < img_points.cols; ++j) {
                     cv::Point img_point = img_points.at<cv::Point2f>(i, j);
                     if (img_point.x <= cv_ptr->image.size().width && img_point.x >= 0 && img_point.y <= cv_ptr->image.size().height && img_point.y >= 0) {
-                        // std::cout << " " << ptr_cloud->points[i].intensity << " " << std::endl;
+                        // auto value = static_cast<unsigned int>((ptr_cloud->points[i].intensity / *(minmax.second)) * 255.0);
 
-                        // @todo intensity to rgb 
-                        cv::Scalar color{ptr_cloud->points[i].intensity, ptr_cloud->points[i].intensity, ptr_cloud->points[i].intensity};
+                        // @todo : Convert intensity to RGB       
+                        auto _intensity = dst.points[i].intensity;
+                        unsigned int b = static_cast<unsigned int>(*(minmax.second) - _intensity);
+                        unsigned int g = static_cast<unsigned int>(_intensity <= *(minmax.second)/2 ? _intensity + *(minmax.second)/2 : *(minmax.second) - _intensity);
+                        unsigned int r = static_cast<unsigned int>(_intensity);
+
+                        // std::cout << "intensity : " << _intensity << std::endl;
+                        // std::cout << "b g r : " << b << " " << g << " " << r << std::endl;
+
+                        // ProjectionPoints _points {img_point, cv::Scalar(g/2, r/2, b)};
                         ProjectionPoints _points {img_point, cv::Scalar(0, 0, 255)};
                         projection_points.push_back(_points);
                     }
                 }
             }
+            is_lidar = true;
         }
-
-        is_lidar = true;
     }
 
     void json_to_mat(const Json::Value &jsonValue, cv::Mat &mat) {
@@ -153,12 +189,26 @@ private:
         }
     }
 
+    sensor_msgs::msg::PointCloud2 cloud2cloudmsg(pcl::PointCloud<pcl::PointXYZI> cloud_src){
+        sensor_msgs::msg::PointCloud2 cloudmsg;
+        pcl::toROSMsg(cloud_src, cloudmsg);
+        cloudmsg.header.frame_id = "os_sensor";
+        return cloudmsg;
+    }
+
+    pcl::PointCloud<pcl::PointXYZI> cloudmsg2cloud(sensor_msgs::msg::PointCloud2 cloudmsg){
+        pcl::PointCloud<pcl::PointXYZI> cloud_dst;
+        pcl::fromROSMsg(cloudmsg, cloud_dst);
+        return cloud_dst;
+    }
+
     cv_bridge::CvImagePtr cv_ptr;
     
     rclcpp::TimerBase::SharedPtr timer_;
 
     rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr img_sub_;
     rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr lidar_sub_;
+    rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr filtered_lidar_pub_;
 
     cv::Mat rvec, tvec, camera_intrinsic_matrix, camera_dist_coeff;
     pcl::PointCloud<pcl::PointXYZI>::Ptr ptr_cloud;
